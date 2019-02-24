29704dbd2c7ed1c195a538ce904621b0d144de5a
conch <102347501@qq.com>
2019-02-24 22:40:47 +0800

    1

---
+++ Web_AdminLTE/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fonour.Application.UserApp;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web_AdminLTE.Controllers
+{
+    public class LoginController : Controller
+    {
+        private IUserAppService _userAppService;
+        public LoginController(IUserAppService userAppService)
+        {
+            _userAppService = userAppService;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var user = _userAppService.CheckUser("admin", "123456");
+            return View();
+        }
+    }
+}
---
+++ Web_AdminLTE/Startup.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Fonour.Application.UserApp;
+using Fonour.Domain.IRepositories;
+using Fonour.EntityFrameworkCore.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Web_AdminLTE.Model;
+
+namespace Web_AdminLTE
+{
+    public class Startup
+    {
+        public IConfigurationRoot Configuration { get; }
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //获取数据库连接字符串
+            var sqlConnectionString = Configuration.GetConnectionString("Default");
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            //添加数据上下文
+            services.AddDbContext<FonourDbContext>(options => options.UseNpgsql(sqlConnectionString));
+
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IUserAppService, UserAppService>();
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                //生产环境异常处理
+                app.UseExceptionHandler("/Shared/Error");
+            }
+            //使用静态文件
+            app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(Directory.GetCurrentDirectory())
+            });
+            //使用Mvc，设置默认路由
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Login}/{action=Index}/{id?}");
+            });
+
+            SeedData.Initialize(app.ApplicationServices.CreateScope().ServiceProvider.GetService<FonourDbContext>()); //初始化数据
+        }
+    }
+}
